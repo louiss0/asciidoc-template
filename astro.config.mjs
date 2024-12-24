92ac7e6a127f8a9c82637d25ef035e4f12b7d080
louiss0 <louisshelton0@gmail.com>
2024-12-23 21:41:25 -0500

    chore: integrate unocss with astro.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+import unocss from '@unocss/astro';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [sitemap()],
+    site: 'https://example.com',
+    integrations: [sitemap(), unocss()],
 });
